3a90c0f0c3787da43f4701f81812b4d0ff298421
gab <gabrielmarianoofficial@gmail.com>
2022-10-04 03:08:54 +0800

    fixed imports again

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as Hero } from "./hero";
 export { default as Links } from "./links";
 export { default as Works } from "./works";
 export { default as Stack } from "./stack";
+export { default as Footer } from "./footer";
