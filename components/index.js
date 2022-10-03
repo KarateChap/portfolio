205a6f68618e00fbc252e5ea5e4fa4a0667e3a84
gab <gabrielmarianoofficial@gmail.com>
2022-10-04 03:05:45 +0800

    fixed imports

--- components/index.js
+++ components/index.js
@@ -2,4 +2,4 @@ export { default as Navbar } from "./navbar";
 export { default as Hero } from "./hero";
 export { default as Links } from "./links";
 export { default as Works } from "./works";
-export { default as Stack } from "./Stack";
+export { default as Stack } from "./stack";
